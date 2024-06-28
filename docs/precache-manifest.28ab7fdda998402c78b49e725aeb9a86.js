e4a8f3b0271126f547b4812a2ee303be6327fe8f
legndery <ar_itra@yahoo.com>
2024-06-28 16:52:41 +0530

    Add build for gh-pages

---
+++ docs/precache-manifest.28ab7fdda998402c78b49e725aeb9a86.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "89889688147bd7575d6327160d64e760.svg"
+  },
+  {
+    "url": "e18bbf611f2a2e43afc071aa2f4e1512.ttf"
+  },
+  {
+    "url": "f4769f9bdb7466be65088239c12046d1.eot"
+  },
+  {
+    "revision": "e449449c264aef78c07bd438c2861df6",
+    "url": "index.html"
+  },
+  {
+    "url": "main.5cd2cede946011c3570f.js"
+  }
+]);
---
+++ docs/sw.js
@@ -0,0 +1,5 @@
+importScripts("precache-manifest.28ab7fdda998402c78b49e725aeb9a86.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+
